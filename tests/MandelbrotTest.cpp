52ad48d2fab6a7e29f7f4ebdd1b458c947c66b19
alexandrebignalet <alexandre.bignalet@viacesi.fr>
2017-03-05 12:16:33 -0500

    adding some tests

--- tests/MandelbrotTest.cpp
+++ tests/MandelbrotTest.cpp
@@ -16,29 +16,48 @@ int main( int argc, char* argv[] )
   return 0;
 }
 
-bool point_egaux(Point &p, Point &p2) {
-  return p.x_pos == p2.x_pos &&
-         p.y_pos == p2.y_pos &&
-         p.zx == p2.zx &&
-         p.zy == p2.zy &&
-         p.last_iter == p2.last_iter;
+bool point_egaux(Point* p, Point* p2) {
+
+  return p->x_pos == p2->x_pos &&
+         p->y_pos == p2->y_pos &&
+         p->zx == p2->zx &&
+         p->zy == p2->zy &&
+         p->last_iter == p2->last_iter;
 }
 
-bool mandelbrot_egaux(MandelbrotDataModel m1, MandelbrotDataModel m2) {
-  if ( !(m1.size() == m2.size() && m1[0].size() != m2[0].size()) ) {
+bool mandelbrot_egaux(MandelbrotDataModel& m1, MandelbrotDataModel& m2) {
+  if ( !(m1.size() == m2.size() && m1[0].size() == m2[0].size()) ) {
     return false;
   }
 
 
   for (unsigned int i = 0; i < m1.size(); i++) {
     for (unsigned int y = 0; y < m1[0].size(); y++) {
-      if (!point_egaux(m1[i][y], m2[i][y]) ) {
+      if (!point_egaux(&m1[i][y], &m2[i][y]) ) {
         return false;
       }
     }
   }
 
   return true;
+}
+
+bool mandelbrot_generes_egaux(int width, int height, int iter_max, double zoom, double x_min, double y_max) {
+  Mandelbrot *mandel = new Mandelbrot(width, height, iter_max, zoom);
+
+  MandelbrotDataModel graph_seq = mandel->initDataModel(x_min,y_max);
+  MandelbrotDataModel graph_par_dyn = mandel->initDataModel(x_min,y_max);
+  MandelbrotDataModel graph_par_static = mandel->initDataModel(x_min,y_max);
+  MandelbrotDataModel graph_par = mandel->initDataModel(x_min,y_max);
+
+  mandel->process_seq(graph_seq);
+  mandel->process_par_dyn(graph_par_dyn);
+  mandel->process_par_static(graph_par_static, graph_par_static.begin(), graph_par_static.end(), 1);
+  mandel->process_par(graph_par);
+
+  return mandelbrot_egaux(graph_seq, graph_par_dyn) &&
+         mandelbrot_egaux(graph_seq, graph_par_static) &&
+         mandelbrot_egaux(graph_seq, graph_par);
 }
 
 // Cas de tests Mandelbrot(int width, int height, int iter_max, double zoom)
@@ -78,20 +97,10 @@ FIN_CAS_DE_TEST
 // Cas de tests la version sequentielle génère bien la fractale voulue.
 // Pour tester les versions parallele on comparera les MandelbrotDataModel resultants
 CAS_DE_TEST( test_generation )
-Mandelbrot *mandel = new Mandelbrot(640, 360, 200, 4.0);
-MandelbrotDataModel graph_seq = mandel->initDataModel(-2,2);
-MandelbrotDataModel graph_par_dyn = mandel->initDataModel(-2,2);
-MandelbrotDataModel graph_par_static = mandel->initDataModel(-2,2);
-MandelbrotDataModel graph_par = mandel->initDataModel(-2,2);
 
-mandel->process_seq(graph_seq);
-mandel->process_par_dyn(graph_par_dyn);
-mandel->process_par_static(graph_par_static, graph_par_static.begin(), graph_par_static.end(), 1);
-mandel->process_par(graph_par);
-
-mandelbrot_egaux(graph_seq, graph_par_dyn);
-mandelbrot_egaux(graph_seq, graph_par_static);
-mandelbrot_egaux(graph_seq, graph_par);
+assertVrai(mandelbrot_generes_egaux(640, 360, 200, 4.0, -2, 2));
+assertVrai(mandelbrot_generes_egaux(1200, 1200, 200, 2.0, -1, 1));
+assertVrai(mandelbrot_generes_egaux(555, 700, 150, 1.0, -0.5, 0.5));
 
 FIN_CAS_DE_TEST
 
